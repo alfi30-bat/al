f407f4983043c473ba668ece1c7d32b9729b8cc0
alfi30-bat <78789080+alfi30-bat@users.noreply.github.com>
2024-07-23 22:29:51 +0530

    Add files via upload

--- user/urls.py
+++ user/urls.py
@@ -9,6 +9,7 @@ from django.conf.urls.static import static
 urlpatterns = [
         path('', views.index, name ='index'),
         path('login/', views.Login, name ='login'),
-        path('logout/', auth.LogoutView.as_view(template_name ='user/index.html'), name ='logout'),
+        path('logout/', auth.LogoutView.as_view(template_name ='index.html'), name ='logout'),
+        #path('user/', include('django.contrib.auth.urls')),
         path('register/', views.register, name ='register'),         
 ]
